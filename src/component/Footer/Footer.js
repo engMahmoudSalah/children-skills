ba8be9b04a79ef2239fe731403070dd9b563ce27
mahmoud salah <140615783+engMahmoudSalah@users.noreply.github.com>
2024-05-12 21:59:27 +0300

    f66

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => {
             <h4 className="h4"> تابعنا</h4>
             <div className="list-unstyled2">
               <li className="li">
-                <a href="https://www.facebook.com/groups/706464223561493/?ref=share&mibextid=NSMWBT">
+                <a href="https://www.facebook.com/profile.php?id=61559589991996&mibextid=ZbWKwL">
                   <i className="fa fa-facebook i"></i>
                 </a>
               </li>
